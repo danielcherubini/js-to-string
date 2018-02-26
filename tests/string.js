3be7e27a803bd37e0c3a20527df70fcea175e98a
Daniel Cherubini <daniel@cherubini.casa>
2018-02-26 17:50:14 +0100

    added symbols

--- tests/string.js
+++ tests/string.js
@@ -46,6 +46,20 @@ test("String", t => {
     t.is(result, expected);
 });
 
+test("Symbol", t => {
+    let obj = {
+        type1: String,
+        type2: Number,
+        type3: Boolean,
+        type4: Function,
+        type5: Object
+    };
+
+    const result = stringit(obj);
+    const expected = "{type1: String,type2: Number,type3: Boolean,type4: Function,type5: Object}";
+    t.is(result, expected);
+});
+
 test("Object", t => {
 
     const objectt = {
